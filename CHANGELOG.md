083a2db1188bb699b4ea33319c29b64e5844a417
a.dzodzikov <anton.dzodzikov@gmail.com>
2018-09-27 16:54:06 +0300

    Add TODOs & update changelog in advance

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,4 @@
 # 1.0
 + Added health endpoint: <code>/status/health</code>
 + Added version endpoint: <code>/status/version</code>
++ Added Operation Template API (read/update/delete)
--- src/main/java/taskmaster/operations/api/OperationTemplateController.java
+++ src/main/java/taskmaster/operations/api/OperationTemplateController.java
@@ -15,17 +15,34 @@ public class OperationTemplateController {
 
     @RequestMapping("")
     public Flux<OperationTemplate> getAll() {
+        // TODO: Add test
         return repository.findAll();
     }
 
     @RequestMapping("{name}")
     public Mono<OperationTemplate> getByName(@PathVariable String name) {
+        // TODO: Add test
         return repository.findFirstByName(name);
     }
 
     @RequestMapping(path = "", method = RequestMethod.PUT)
     public Mono<OperationTemplate> createOperationTemplate(@RequestBody OperationTemplate template) {
         // TODO: Allow this method to do multiple insertions (as list)
+        // TODO: Add test
         return repository.save(template);
+    }
+
+    @RequestMapping(path = "", method = RequestMethod.POST)
+    public Mono<OperationTemplate> updateOperationTemplate(@RequestBody OperationTemplate template) {
+        // TODO: Allow this method to do multiple updates (as list)
+        // TODO: Add test
+        return null;
+    }
+
+    @RequestMapping(path = "", method = RequestMethod.DELETE)
+    public Mono<OperationTemplate> deleteOperationTemplate(@RequestBody OperationTemplate template) {
+        // TODO: Allow this method to do multiple deletions (as list)
+        // TODO: Add test
+        return null;
     }
 }
